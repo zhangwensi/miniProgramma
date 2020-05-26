aeaf5de0132facb02c7434734ab93ff8ce95ca33
Your Name <you@example.com>
2020-05-27 00:25:02 +0800

    上传小程序login页面

---
+++ miniprogram-4/pages/03-login/03-login.js
@@ -0,0 +1,80 @@
+// pages/03-login/03-login.js
+Page({
+  _submit:function (evt) {
+    console.log(evt)
+  },
+  _checkL:function (evt) {
+    let reult = evt.detail.value
+      this.setData({
+        pass:reult.length >= 4
+    })
+  },
+  _showP: function () {
+    this.setData({
+      showPass:!this.data.showPass
+    })
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pass:false,
+    showPass:false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
